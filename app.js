d847651b310c843f0fdefcf61a3de459dba9e7c4
stevenjchang <stevenjchang@gmail.com>
2020-10-17 13:11:29 -0700

    fix: add CORS headers

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ const listingRouter = require("./routes/listings.js");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 var router = express.Router();
 
-router.get("/", function (req, res) {
-  res.send("im the home page!");
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-router.get("/about", function (req, res) {
-  res.send("im the about page!");
+router.get("/", (req, res) => {
+  res.send("im the home page!");
 });
 
 app.use("/listing", listingRouter);
