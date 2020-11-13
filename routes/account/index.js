8b5659782c08cb25c3ff316a6d39cb6a040ac96a
stevenjchang <stevenjchang@gmail.com>
2020-11-13 14:54:48 -0800

    add Account.createUser

--- routes/account/index.js
+++ routes/account/index.js
@@ -14,7 +14,7 @@ router.get("/allusers", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const user = await Account.findUser(id);
+  const user = await Account.findUserById(id);
   if (user.length === 1) {
     res.json(user);
   } else if (user.length > 1) {
@@ -24,29 +24,25 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/add", async (req, res) => {
-  const selectExistsQuery =
-    "SELECT EXISTS (SELECT username FROM account WHERE username = $1)";
-  const insertIntoQuery =
-    "INSERT INTO account (username, email, display_name) VALUES ($1, $2, $3) RETURNING *";
-
-  const { username, email, display_name } = req.body;
-
+router.post("/createuser", async (req, res) => {
+  const { email } = req.body;
   try {
-    const ifExistsResult = await client.query(selectExistsQuery, [username]);
-    const alreadyExists = ifExistsResult.rowCount > 0;
-    if (alreadyExists) {
-      res.status(400).send(`sorry, the username "${username}" is already take`);
+    const existingUser = await Account.findUserByEmail(email);
+    console.log("existingUser ==>", existingUser);
+    if (existingUser.length === 0) {
+      const newUserInfo = await Account.createUser(req.body);
+      if (newUserInfo.name === "error") {
+        res.send(newUserInfo.message);
+      } else {
+        res.json(newUserInfo);
+      }
     } else {
-      const dbRes = await client.query(insertIntoQuery, [
-        username,
-        email,
-        display_name,
-      ]);
-      res.send({ result: dbRes.rows });
+      res.send(
+        `a user with this email already exists: ${existingUser[0].email}`
+      );
     }
   } catch (err) {
-    console.log("Error ==>", err);
+    console.log("err ==>", err);
   }
 });
 
